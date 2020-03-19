cb865c12bfd40dcfbc6394ce025421d4102b019a
Unknown <grimmring@aol.com>
2020-03-19 16:28:37 +0100

    fixed warnings

--- PIPAlgorithms/CUDA/DisparityEstimation_basic.hh
+++ PIPAlgorithms/CUDA/DisparityEstimation_basic.hh
@@ -67,9 +67,9 @@ public:
         m_params.descrMla = descrMLA;
 
         // Direct parameters settings
-        m_params.fMinDisparity = StdMapTestAndGet(mapAdditionalParams, "Min Disparity");
-        m_params.fMaxDisparity = StdMapTestAndGet(mapAdditionalParams, "Max Disparity");
-        m_params.fMinCurvature = StdMapTestAndGet(mapAdditionalParams, "Min Curvature");
+        m_params.fMinDisparity = float(StdMapTestAndGet(mapAdditionalParams, "Min Disparity"));
+        m_params.fMaxDisparity = float(StdMapTestAndGet(mapAdditionalParams, "Max Disparity"));
+        m_params.fMinCurvature = float(StdMapTestAndGet(mapAdditionalParams, "Min Curvature"));
     }
 
     ///
