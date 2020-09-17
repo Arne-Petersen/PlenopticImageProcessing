21e21bb8408d8b47969a87b87bb48d7cf87550aa
Arne Petersen <grimmring@aol.com>
2020-09-17 11:33:47 +0200

    fixed size of shared CUDA mem for intial disparity estimation

--- PIPAlgorithms/CUDA/DisparityEstimation_OFL.cu
+++ PIPAlgorithms/CUDA/DisparityEstimation_OFL.cu
@@ -568,12 +568,12 @@ inline void StartDisparityKernel(const dim3 lensDims, const dim3 threadsPerLensD
         const vec2<float> vPixelOffset_px)
 {
 	// shared memory for lens offsets and epi lines
-	const int intSharedMem = 6 * 2 * sizeof(float);
+	const int intSharedMem = 6 * 2 * 2 * sizeof(float);
 
     // Call kernel with appropriate channel count
     if (intChannelCount == 1)
     {
-        computeDisparity<DISPSTEPS_INITIAL, HWS_INITIAL, 1, t_eGridType><<<lensDims, threadsPerLensDims>>>(arrOutput.GetDevicePointer(),
+        computeDisparity<DISPSTEPS_INITIAL, HWS_INITIAL, 1, t_eGridType><<<lensDims, threadsPerLensDims, intSharedMem >>>(arrOutput.GetDevicePointer(),
                                                                                                            arrOutWeightSum.GetDevicePointer(),
                                                                                                            texInput.GetTextureObject(),
                                                                                                            vGridCenerBlock,
@@ -581,7 +581,7 @@ inline void StartDisparityKernel(const dim3 lensDims, const dim3 threadsPerLensD
     }
     else if (intChannelCount == 2)
     {
-        computeDisparity<DISPSTEPS_INITIAL, HWS_INITIAL, 2, t_eGridType><<<lensDims, threadsPerLensDims>>>(arrOutput.GetDevicePointer(),
+        computeDisparity<DISPSTEPS_INITIAL, HWS_INITIAL, 2, t_eGridType><<<lensDims, threadsPerLensDims, intSharedMem >>>(arrOutput.GetDevicePointer(),
                                                                                                            arrOutWeightSum.GetDevicePointer(),
                                                                                                            texInput.GetTextureObject(),
                                                                                                            vGridCenerBlock,
